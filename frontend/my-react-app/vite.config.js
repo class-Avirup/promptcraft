e865af2c70e137e5a43f1a8f4e53cca4007b3d22
Avirup <132709541+class-Avirup@users.noreply.github.com>
2025-07-01 19:50:12 +0530

    vite error solved

--- frontend/my-react-app/vite.config.js
+++ frontend/my-react-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH||"/"
+  base: "/"
 })
