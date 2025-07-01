6f5fa3d2b054d8c4227f3716cbb83e83aae67b90
Avirup <132709541+class-Avirup@users.noreply.github.com>
2025-07-01 17:17:09 +0530

    removed unneccesary files

--- frontend/my-react-app/src/components/archive.jsx
+++ frontend/my-react-app/src/components/archive.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import PromptCard from "../components/PromptCard";
+import PromptCard from "./PromptCard";
 import axios from "axios";
 
 const TAGS = [
